b9e13e336ff548bbc730b94c13651f14d0688de7
Radcabna <radcabna@gmail.com>
2026-04-29 12:19:01 +0300

    Initial Commit

---
+++ Merkur Casino/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Merkur Casino
+//
+//  Created by Алкександр Степанов on 29.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Merkur Casino/Merkur_CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Merkur_CasinoApp.swift
+//  Merkur Casino
+//
+//  Created by Алкександр Степанов on 29.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Merkur_CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
